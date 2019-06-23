8997b17302dbba53e1fc30aa27add118d35ee63d
Daniel Bedoya <danielbedoyatbs@utp.edu.co>
2019-06-23 09:59:20 -0500

    Commit with the montecarlo needles algorithm with mpi

---
+++ montecarlo_needles.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#include <random>
+#include "mpi.h"
+
+using namespace std;
+
+double pi = 3.14159265;
+
+bool check_intersect(double x, double theta, int l, int s){
+    //cout << x << endl;
+    //cout << theta << endl;
+    if(x + (s/2.0)*sin(theta) >= l)return true;
+    if(x - (s/2.0)*sin(theta) <= 0.0)return true;
+    return false;
+}
+
+int monte_carlo(int t, int l, int s){
+    int total = 0;
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<> dis(0.0, 1.0);
+    int i;
+    double x0;
+    double theta;
+    for(i = 0; i < t; i++){       
+        x0 = l*dis(gen);
+        theta = pi*dis(gen);
+        if(check_intersect(x0,theta,l,s)){
+            total += 1.0;
+        }
+    }
+    return total;
+    return (2.0*s*t)/(l*total);
+}
+
+int main(int argc, char *argv[]){
+    int n, t, l, s, rank, size, pi2, pi2total=0, rc;
+    double t1, t2, tf;   // number of throws, dist between lines and 
+    t = (argc > 2 ? atoi(argv[2]) : 100000);
+    l = (argc > 3 ? atoi(argv[3]) : 30);
+    s = (argc > 4 ? atoi(argv[4]) : 10);
+    
+    t1=MPI_Wtime();
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD,&size);
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+    if(rank==0){
+        pi2 = monte_carlo(t-((t/3)*3), l, s);
+    }else{
+        pi2 = monte_carlo(t/3, l, s);
+    }
+
+    rc = MPI_Reduce(&pi2,&pi2total,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+
+    MPI_Finalize();
+    t2=MPI_Wtime();
+
+    if(rank==0){
+        double pi3 = (4.0*double(actotal))/double(t);
+        tf=t2-t1;
+        cout << t <<";"<< fixed << setprecision(10) << pi3 << ";" << tf << endl;
+    }
+    return 0;
+}
