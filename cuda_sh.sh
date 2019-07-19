1719ee9c69c0790a7ee00b02a0eb7e6dfc1829f6
Gustavo <gustavogarcia@utp.edu.co>
2019-07-19 00:02:51 -0500

    commit from node4

---
+++ cuda_sh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for k in {1..5}
+do
+	for j in {5..10}
+	do
+		for i in {50 200 400 600 800 1000 3000 5000 8000 10000 40000 70000 100000 200000 500000 800000 1000000}
+		do
+			./secuencial $i $j >> resultadosec2.csv
+		done
+	done
+done
